8d74f856b438ed5fee742451696362d12c131e20
547092 <manas.niet@hotmail.com>
2016-04-29 14:49:06 +0530

    Delete fix

--- Backend/spring-boot-poc/src/main/java/app/DemoController.java
+++ Backend/spring-boot-poc/src/main/java/app/DemoController.java
@@ -1,6 +1,7 @@
 package app;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,19 +44,17 @@ public class DemoController
 		return "Success";
 	}
 
-	@RequestMapping(value="/delete", method={RequestMethod.POST})
-	public boolean deleteDemoBean(@RequestBody Long id)
+	@RequestMapping(value="/delete", method={RequestMethod.POST}, consumes = "application/x-www-form-urlencoded", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Boolean deleteDemoBean(@RequestParam("id") long id)
 	{
 		DemoBean beanToDelete = repo.findOne(id);
 		if(beanToDelete == null)
-			//return "Bean with Id : "+id+" Not found in the database!";
 			return false;
 		else
 		{
 			repo.delete(beanToDelete);
 			return true;
 		}
-		//return "Bean with Id : "+id+" successfully deleted!";
 
 	}
 }
