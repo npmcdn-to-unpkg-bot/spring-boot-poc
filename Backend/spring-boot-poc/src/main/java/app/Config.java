1661b7af9cab5852a60dbfe4cc3d84844cf2e709
547092 <manas.niet@hotmail.com>
2016-06-03 16:17:32 +0530

    Added MongoDB bean

--- Backend/spring-boot-poc/src/main/java/app/Config.java
+++ Backend/spring-boot-poc/src/main/java/app/Config.java
@@ -1,17 +1,32 @@
 package app;
 
-import org.h2.server.web.WebServlet;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
+//import org.h2.server.web.WebServlet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 
 @Configuration
-public class Config 
+public class Config extends AbstractMongoConfiguration
 {
-	@Bean
+	/*@Bean
 	public ServletRegistrationBean h2servletRegistration() {
-	    ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
-	    registration.addUrlMappings("/console/*");
-	    return registration;
+		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+		registration.addUrlMappings("/console/*");
+		return registration;
+	}*/
+
+	@Override
+	public String getDatabaseName() {
+		return "manas";
 	}
+
+	@Override
+	@Bean
+	public Mongo mongo() throws Exception {
+		return new MongoClient("127.0.0.1");
+	}
+
 }
